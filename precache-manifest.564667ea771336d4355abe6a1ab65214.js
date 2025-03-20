120c32066753d96448b769aa5d0c7f2bf833c6af
Devesh Mehta <devesh1217@gmail.com>
2025-03-20 21:47:42 +0530

    Updates

---
+++ precache-manifest.564667ea771336d4355abe6a1ab65214.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cdb0ea22193be880701d3d684aeb62",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9168847647a9a537b4c3",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b4d8c47d4b998bcbcfd2",
+    "url": "/portfolio/static/css/main.adce1b9f.chunk.css"
+  },
+  {
+    "revision": "9168847647a9a537b4c3",
+    "url": "/portfolio/static/js/2.76471cc2.chunk.js"
+  },
+  {
+    "revision": "b9afd0b4a4e35d4a7a10",
+    "url": "/portfolio/static/js/3.b52b6af5.chunk.js"
+  },
+  {
+    "revision": "b4d8c47d4b998bcbcfd2",
+    "url": "/portfolio/static/js/main.69bc7536.chunk.js"
+  },
+  {
+    "revision": "1556738260a98c3c03e6",
+    "url": "/portfolio/static/js/runtime~main.74f1f0ac.js"
+  },
+  {
+    "revision": "4e9beaa6c1eea560a18dcac9e7dced07",
+    "url": "/portfolio/static/media/arduino.4e9beaa6.png"
+  },
+  {
+    "revision": "cb226e670b5e76233ea953618aedfc4b",
+    "url": "/portfolio/static/media/clipart.cb226e67.webp"
+  },
+  {
+    "revision": "7b711a98365332aeaa71d2e7d3e366aa",
+    "url": "/portfolio/static/media/css.7b711a98.png"
+  },
+  {
+    "revision": "8a0c76bd52bcb97d4a780a69225d410c",
+    "url": "/portfolio/static/media/download.8a0c76bd.png"
+  },
+  {
+    "revision": "b76c230c3a071842beb4b793baaac0ab",
+    "url": "/portfolio/static/media/email.b76c230c.webp"
+  },
+  {
+    "revision": "dd5571e4b77271f5bae12c1d19f8443b",
+    "url": "/portfolio/static/media/git.dd5571e4.png"
+  },
+  {
+    "revision": "5e72d5645c0e4adfa4ee561253686986",
+    "url": "/portfolio/static/media/healthify.in.5e72d564.png"
+  },
+  {
+    "revision": "2169b9f58cedc2229ab27eeb81b454e4",
+    "url": "/portfolio/static/media/html.2169b9f5.png"
+  },
+  {
+    "revision": "875f86a77adeae947912976f1373a3a9",
+    "url": "/portfolio/static/media/insta.875f86a7.webp"
+  },
+  {
+    "revision": "b6de03d7686a5c9f6ee84cbc2598792b",
+    "url": "/portfolio/static/media/js.b6de03d7.png"
+  },
+  {
+    "revision": "717ef619412e74acc412b7cb61a0c080",
+    "url": "/portfolio/static/media/line follower1.717ef619.jpg"
+  },
+  {
+    "revision": "36f45d50a5f82ab30c534d9f715e30f6",
+    "url": "/portfolio/static/media/linkedin.36f45d50.png"
+  },
+  {
+    "revision": "40415d5226994643c9a73999587f92db",
+    "url": "/portfolio/static/media/portfolio.40415d52.png"
+  },
+  {
+    "revision": "ad72ad90292660ea5049b0d6e29281b3",
+    "url": "/portfolio/static/media/profile-pic2.ad72ad90.png"
+  },
+  {
+    "revision": "6d6d5986fc56e8f6c7265ce07e31a95a",
+    "url": "/portfolio/static/media/reactIcon.6d6d5986.png"
+  },
+  {
+    "revision": "b491f41b06d67397a601dba504cce37e",
+    "url": "/portfolio/static/media/top.b491f41b.png"
+  }
+]);
